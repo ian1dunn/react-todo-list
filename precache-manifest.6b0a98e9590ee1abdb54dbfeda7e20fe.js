8db2669386f688fd05016b1a8abe8b0dc75213cd
Trifex <techwizguy7@gmail.com>
2020-04-05 13:54:50 -0400

    Updates

---
+++ precache-manifest.6b0a98e9590ee1abdb54dbfeda7e20fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf9913027893d02665ac0afcaf9fd0f",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "c04835d96c21d5c4f40d",
+    "url": "/react-todo-list/static/css/main.b5b0c93c.chunk.css"
+  },
+  {
+    "revision": "ec52275e0d6c31863117",
+    "url": "/react-todo-list/static/js/2.77dc2f55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.77dc2f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04835d96c21d5c4f40d",
+    "url": "/react-todo-list/static/js/main.760e8746.chunk.js"
+  },
+  {
+    "revision": "8e9c983315010bc1ad3a",
+    "url": "/react-todo-list/static/js/runtime-main.e4f00cf7.js"
+  }
+]);
